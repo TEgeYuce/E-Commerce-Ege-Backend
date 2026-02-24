6461f0f17c07c23dd7df87e60c76665266d81bab
TEgeYuce <tegeyucesan@gmail.com>
2026-02-24 21:20:53 +0300

    Proje Setup Tamamlandı

---
+++ ecommege/src/main/java/com/example/workintech/ecommege/ECommerceEgeApp.java
@@ -0,0 +1,14 @@
+package com.example.workintech.ecommege;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+
+@SpringBootApplication
+public class ECommerceEgeApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceEgeApp.class, args);
+    }
+
+
+}
+
